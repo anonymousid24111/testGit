f71aaab7bec421c0c03d5365bac22f85295e4183
giangnd <nguyenducgiang2411@gmaill.com>
2021-06-29 11:42:18 +0700

    re

--- a.js
+++ a.js
@@ -1,18 +1,3 @@
 console.log("aaa")
 
 console.log("ggggg")
-
-
-
-
-
-console.log("bbbbb")
-console.log("CCCC")
-
-console.log("bbbbb")
-console.log("CCCC")
-
-
-console.log("bbbbb")
-console.log("CCCC")
-    
