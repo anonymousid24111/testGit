e09491d46169331968f4ef707be9b58d964005dc
giangnd <nguyenducgiang2411@gmaill.com>
2021-06-29 11:43:05 +0700

    gagag

--- a.js
+++ a.js
@@ -2,17 +2,4 @@ console.log("aaa")
 
 console.log("ggggg")
 
-
-
-
-
-console.log("bbbbb")
-console.log("CCCC")
-
-console.log("bbbbb")
-console.log("CCCC")
-
-
-console.log("bbbbb")
-console.log("CCCC")
-    
+console.log("hahah")
